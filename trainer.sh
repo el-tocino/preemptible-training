8734173a79665173cc3a3f89d1f9fa6c0c224ee9
el-tocino <email+github@aforkinthetoad.com>
2020-08-13 10:27:54 -0500

    Update trainer.sh

--- trainer.sh
+++ trainer.sh
@@ -3,7 +3,7 @@ sleep 1
 cd /home/$USER/TTS/
 TCMD="python3 train.py --config_path /home/professorevil/GA/config.json --restore_path /home/professorevil/GA/lates
 t/best_model.pth.tar"
-RUN_CMD="screen -mdS train  ${TCMD}"
+RUN_CMD="screen -S train -md ${TCMD}"
 echo "running: $RUN_CMD" >> /home/professorevil/logs/train.log
 $RUN_CMD
 RESULT=$(echo $?)
